93cbe415c585260c7120198b569d43b2a5fa309e
Ivancho <ivan.quispe.chura@outlook.com>
2022-12-25 15:31:23 -0400

    Day 01

---
+++ AdventOfCode/Day01/Solultion.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode.Day01
+{
+    public class Solution
+    {
+        public static int Part1(string path, string filename)
+        {
+            List<int> totalCaloriesByElf = ReadInput(path, filename);
+            return totalCaloriesByElf.OrderByDescending(tc => tc).Take(1).Sum();
+        }
+
+        public static int Part2(string path, string filename)
+        {
+            List<int> totalCaloriesByElf = ReadInput(path, filename);
+            return totalCaloriesByElf.OrderByDescending(tc => tc).Take(3).Sum();
+        }
+        static List<int> ReadInput(string path, string filename)
+        {
+            string[] input = File.ReadAllText(Path.Combine(path, filename)).Split("\r\n\r\n");
+            List<int> elfs = new();
+            foreach (var calories in input)
+            {
+                elfs.Add(calories.Split("\r\n").Select(x => int.Parse(x)).ToArray().Sum());
+            }
+            return elfs;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -6,7 +6,16 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Advent Of Code!");
+            string problemDay = "01";
+
+            // test.txt -> To test data from example
+            // input.txt -> To solve the real challenge
+            string input = "input.txt";
+
+            string path = Path.Combine(Environment.CurrentDirectory, "Day" + problemDay);
+
+            Console.WriteLine($"Part 1: {Day01.Solution.Part1(path, input)}");
+            Console.WriteLine($"Part 2: {Day01.Solution.Part2(path, input)}");
         }
     }
 }
