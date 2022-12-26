8f4618a9f60664aa8854ca440450a80b73cb4387
Ivancho <ivan.quispe.chura@outlook.com>
2022-12-26 15:45:10 -0400

    Day 03

---
+++ AdventOfCode/Day03/Solultion.cs
@@ -0,0 +1,50 @@
+﻿namespace AdventOfCode.Day03
+{
+    public class Solution
+    {
+        static Dictionary<char, int> chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            .Select((c, i) => new { Key = (char)c, Value = i + 1 })
+            .ToDictionary(x => x.Key, x => x.Value);
+
+        public static int Part1(string path, string filename)
+        {
+            List<Tuple<char[], char[]>> rucksacks = new List<Tuple<char[], char[]>>();
+            var input = ReadInput(path, filename);
+            foreach (var rucksack in input)
+            {
+                int lengthCompartment = rucksack.Length / 2;
+                var compartment1 = rucksack.Substring(0, lengthCompartment).ToCharArray();
+                var compartment2 = rucksack.Substring(lengthCompartment, lengthCompartment).ToCharArray();
+                var tupla = Tuple.Create(compartment1, compartment2);
+                rucksacks.Add(tupla);
+            }
+
+            int priority = 0;
+            foreach (var rucksack in rucksacks)
+            {
+                char commonItem = rucksack.Item1.Intersect(rucksack.Item2).FirstOrDefault();
+                priority += chars[commonItem];
+            }
+
+            return priority;
+        }
+
+        public static int Part2(string path, string filename)
+        {
+            List<string[]> groups = ReadInput(path, filename).Chunk(3).ToList();
+            int priority = 0;
+            foreach (var group in groups)
+            {
+                char commonItem = group[0].Intersect(group[1]).Intersect(group[2]).FirstOrDefault();
+                priority += chars[commonItem];
+            }
+
+            return priority;
+        }
+        
+        static List<string> ReadInput(string path, string filename)
+        {
+            return File.ReadAllText(Path.Combine(path, filename)).Split("\r\n").ToList();
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            string problemDay = "02";
+            string problemDay = "03";
 
             // test.txt -> To test data from example
             // input.txt -> To solve the real challenge
@@ -14,8 +14,8 @@ namespace AdventOfCode
 
             string path = Path.Combine(Environment.CurrentDirectory, "Day" + problemDay);
 
-            Console.WriteLine($"Part 1: {Day02.Solution.Part1(path, input)}");
-            Console.WriteLine($"Part 2: {Day02.Solution.Part2(path, input)}");
+            Console.WriteLine($"Part 1: {Day03.Solution.Part1(path, input)}");
+            Console.WriteLine($"Part 2: {Day03.Solution.Part2(path, input)}");
         }
     }
 }
