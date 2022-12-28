4608bbadd5e6d1d0bc93b406cfcbe1a7aa4b1362
Ivancho <ivan.quispe.chura@outlook.com>
2022-12-28 14:14:02 -0400

    Day 04

---
+++ AdventOfCode/Day04/Solultion.cs
@@ -0,0 +1,82 @@
+﻿namespace AdventOfCode.Day04
+{
+    public class Solution
+    {
+        public static int Part1(string path, string filename)
+        {
+            var tuplas = ReadInput(path, filename);
+
+            int containedCount = 0;
+
+            foreach (var item in tuplas)
+            {
+                containedCount += AnyIsFullyContained(item) ? 1 : 0;
+            }
+            return containedCount;
+        }
+
+        public static int Part2(string path, string filename)
+        {
+            var tuplas = ReadInput(path, filename);
+
+            int containedCount = 0;
+
+            foreach (var item in tuplas)
+            {
+                containedCount += AnyIsPartiallyContained(item) ? 1 : 0;
+            }
+            return containedCount;
+        }
+        
+        private static List<Tuple<Tuple<int, int>, Tuple<int, int>>> ReadInput(string path, string filename)
+        {
+            List<Tuple<Tuple<int, int>, Tuple<int, int>>> assigmentPairs = new();
+            var input = File.ReadAllText(Path.Combine(path, filename)).Split("\r\n").ToList();
+            foreach (var intervals in input)
+            {
+                Tuple<string?, string?> intervalPairs = Tuple.Create
+                    (
+                    intervals.Split(',').ToList().FirstOrDefault(),
+                    intervals.Split(',').ToList().LastOrDefault()
+                    );
+                assigmentPairs.Add(CreateInterval(intervalPairs));
+            }
+            return assigmentPairs;
+        }
+
+        private static Tuple<Tuple<int, int>, Tuple<int, int>> CreateInterval(Tuple<string, string> intervals)
+        {
+            List<int> firstInterval = intervals.Item1.Split('-').Select(n => int.Parse(n)).ToList();
+            var intervalOne = Tuple.Create(firstInterval.FirstOrDefault(), firstInterval.LastOrDefault());
+            List<int> secondInterval = intervals.Item2.Split('-').Select(n => int.Parse(n)).ToList();
+            var intervalTwo = Tuple.Create(secondInterval.FirstOrDefault(), secondInterval.LastOrDefault());
+            return Tuple.Create(intervalOne, intervalTwo);
+        }
+
+        private static bool AnyIsFullyContained(Tuple<Tuple<int, int>, Tuple<int, int>> intervals)
+        {
+            if (intervals.Item1.Item1 <= intervals.Item2.Item1 && intervals.Item1.Item2 >= intervals.Item2.Item2)
+            {
+                return true;
+            }
+            if (intervals.Item2.Item1 <= intervals.Item1.Item1 && intervals.Item2.Item2 >= intervals.Item1.Item2)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool AnyIsPartiallyContained(Tuple<Tuple<int, int>, Tuple<int, int>> intervals)
+        {
+            if (intervals.Item1.Item1 <= intervals.Item2.Item1 && intervals.Item2.Item1 <= intervals.Item1.Item2)
+            {
+                return true;
+            }
+            if (intervals.Item2.Item1 <= intervals.Item1.Item1 && intervals.Item1.Item1 <= intervals.Item2.Item2)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            string problemDay = "03";
+            string problemDay = "04";
 
             // test.txt -> To test data from example
             // input.txt -> To solve the real challenge
@@ -14,8 +14,8 @@ namespace AdventOfCode
 
             string path = Path.Combine(Environment.CurrentDirectory, "Day" + problemDay);
 
-            Console.WriteLine($"Part 1: {Day03.Solution.Part1(path, input)}");
-            Console.WriteLine($"Part 2: {Day03.Solution.Part2(path, input)}");
+            Console.WriteLine($"Part 1: {Day04.Solution.Part1(path, input)}");
+            Console.WriteLine($"Part 2: {Day04.Solution.Part2(path, input)}");
         }
     }
 }
