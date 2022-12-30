54b87a48a87746c2ccefb93c12f208dceb5d52d5
Ivancho <ivan.quispe.chura@outlook.com>
2022-12-30 18:42:22 -0400

    Day 06

---
+++ AdventOfCode/Day06/Solultion.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace AdventOfCode.Day06
+{
+    public class Solution
+    {
+        public static int Part1(string path, string filename)
+        {
+            List<char> letters = ReadInput(path, filename);
+            if (letters.Count < 4)
+            {
+                return 0;
+            }
+            for (int i = 0; i < letters.Count - 4; i++)
+            {
+                if (VerifyDistincts(letters.GetRange(i, 4)))
+                {
+                    return i + 4;
+                }
+            }
+            return 0;
+        }
+
+        public static int Part2(string path, string filename)
+        {
+            List<char> letters = ReadInput(path, filename);
+            if (letters.Count < 14)
+            {
+                return 0;
+            }
+            for (int i = 0; i < letters.Count - 14; i++)
+            {
+                if (VerifyDistincts(letters.GetRange(i, 14)))
+                {
+                    return i + 14;
+                }
+            }
+            return 0;
+        }
+        
+        private static List<char> ReadInput(string path, string filename)
+        {
+            return File.ReadAllText(Path.Combine(path, filename)).ToList();            
+        }
+
+        private static bool VerifyDistincts(List<char> range)
+        {
+            for (int i = 0; i < range.Count; i++)
+            {
+                for (int j = i + 1; j < range.Count; j++)
+                {
+                    if (range[i] == range[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            
+            return true;
+        }
+    }
+}
